1479f285871cdb522d77d5b42958756e58d541a9
Rahat <rahat392@gmail.com>
2019-06-03 19:42:44 +0600

    Commited once more. ;D

--- quicksort.dart
+++ quicksort.dart
@@ -1,9 +1,28 @@
+List swap (List arr, pos_a, pos_b){
+  
+  var temp = arr[pos_a];
+  arr[pos_a] = arr[pos_b];
+  arr[pos_b] = temp;
+  
+  return arr;
+  
+}
+
 int partition(List arr,int low,int high){
   
   int i = low - 1;
   var pivot = arr[high];
+ 	
+  for(int j=low ;j<=high-1;j++){
+    if (arr[j] <= pivot){
+      i++;
+      arr = swap(arr,i,j);
+    }
+  }
+  swap(arr,i+1,high);
   
-  
+  return i+1;
+ 
 }
 
 void quickSort(List arr,int low,int high){
@@ -14,14 +33,20 @@ void quickSort(List arr,int low,int high){
     
     quickSort(arr,low,pi-1);
     quickSort(arr,pi+1,high);
-
+		
   }
 }
 
+List <int> arr = [10, 7, 8, 9, 1, 5];
 void main() {
- 	List <int> arr = [10, 7, 8, 9, 1, 5];
+//  	List <int> arr = [10, 7, 8, 9, 1, 5];
   
   int n = arr.length;
   
   quickSort(arr,0,n-1);
+  
+  print('Sorted List:');
+  print(arr);
+  
+  
 }
